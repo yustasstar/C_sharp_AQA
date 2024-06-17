bd8ae9f21984215edc295812e0c005f8c8bb6579
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-06-17 14:33:12 +0300

    VerifyTextClearFullName

--- PlaywrigthUITests/Tests/TextBoxTests.cs
+++ PlaywrigthUITests/Tests/TextBoxTests.cs
@@ -32,7 +32,6 @@ namespace PlaywrigthUITests.Tests
         }
 
         [Test]
-        [Order(1)]
         [Description("Enter 'Test Name 123' in Text Full Name Input, press submit, text Name should be 'Name:John Doe'")]
         public async Task VerifyFilledTextFullName()
         {
@@ -46,15 +45,21 @@ namespace PlaywrigthUITests.Tests
         }
 
         [Test]
-        [Order(2)]
         [Description("Clear Text Full Name Input, press submit, text Name should not be visible")]
         public async Task VerifyTextClearFullName()
         {
             await _TextBoxPage.GoToTextBoxPage();
+            await _TextBoxPage.FillFullName("Test Name 123");
+            await _TextBoxPage.isFullNameFocused();
+            await _TextBoxPage.ClickSubmitButton();
+            await _TextBoxPage.isSubmitButtonFocused();
+            var isVisible = await _TextBoxPage.IsNameVisible("Test Name 123");
+            Assert.That(isVisible, Is.True, "The element with text 'Name:Test Name 123' should be visible.");
+
+            await _TextBoxPage.GoToTextBoxPage();
             await _TextBoxPage.ClearFullNameInput();
             await _TextBoxPage.ClickSubmitButton();
             await _TextBoxPage.isSubmitButtonFocused();
-            var isVisible = await _TextBoxPage.IsNameHidden("Test Name 123");
             Assert.That(isVisible, Is.True, "The element with text 'Name:Test Name 123' should not be visible.");
         }
     }
