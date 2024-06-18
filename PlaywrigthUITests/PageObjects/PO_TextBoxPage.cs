1f8c6b1cb09787e66db2e4078147de0f613dfccc
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-06-18 13:43:41 +0300

    update TextBox Page tests

--- PlaywrigthUITests/PageObjects/PO_TextBoxPage.cs
+++ PlaywrigthUITests/PageObjects/PO_TextBoxPage.cs
@@ -17,29 +17,29 @@ namespace PlaywrigthUITests.PageObjects
         {
             this.page = page;
         }
-        public async Task GoToTextBoxPage(string testPageUrl)
+        public async Task GoToURL(string testPageUrl)
         {
             await page.GotoAsync(testPageUrl);
-            //await page.WaitForURLAsync(testPageUrl);
+            //await page.WaitForURLAsync(PageUrl);
         }
 
-        public async Task IsTextBoxPageH1Visible(string testPageH1)
+        public async Task IsPageH1Visible(string PageH1)
         {
-            await Assertions.Expect(page.GetByRole(AriaRole.Heading, new() { Name = testPageH1 })).ToBeVisibleAsync();
+            await Assertions.Expect(page.GetByRole(AriaRole.Heading, new() { Name = PageH1 })).ToBeVisibleAsync();
         }
 
-        public async Task IsFullNameLabelVisible(string fullNameLabel)
+        public async Task IsInputLabelVisible(string fullNameLabel)
         {
             await Assertions.Expect(page.Locator("#userName-wrapper div").Filter(new() { HasText = fullNameLabel })).ToBeVisibleAsync();
         }
 
-        public async Task FillFullName(string testFullName, string fullNamePlaceholder)
+        public async Task FillInput(string FullName, string fullNamePlaceholder)
         {
-            await page.GetByPlaceholder(fullNamePlaceholder).FillAsync(testFullName);
-            await Assertions.Expect(page.GetByPlaceholder(fullNamePlaceholder)).ToHaveValueAsync(testFullName);
+            await page.GetByPlaceholder(fullNamePlaceholder).FillAsync(FullName);
+            await Assertions.Expect(page.GetByPlaceholder(fullNamePlaceholder)).ToHaveValueAsync(FullName);
         }
 
-        public async Task IsFullNameFocused(string fullNamePlaceholder)
+        public async Task IsInputFocused(string fullNamePlaceholder)
         {
             await page.GetByPlaceholder(fullNamePlaceholder).ClickAsync();
             await Assertions.Expect(page.GetByPlaceholder(fullNamePlaceholder)).ToBeFocusedAsync();
@@ -52,23 +52,24 @@ namespace PlaywrigthUITests.PageObjects
 
         public async Task IsButtonFocused(string btnName)
         {
-            var submitButton = page.GetByRole(AriaRole.Button, new() { Name = btnName });
-            await submitButton.ClickAsync();
-            await Assertions.Expect(submitButton).ToBeFocusedAsync();
+            var button = page.GetByRole(AriaRole.Button, new() { Name = btnName });
+            await button.ClickAsync();
+            await Assertions.Expect(button).ToBeFocusedAsync();
         }
 
-        public async Task ClearFullNameInput(string fullNamePlaceholder)
+        public async Task ClearInput(string fullNamePlaceholder)
         {
             await page.GetByPlaceholder(fullNamePlaceholder).ClearAsync();
         }
 
-        public async Task VerifyFullNameOutput(string testFullName)
+        public async Task VerifyOutputValue(string outputValue)
         {
-            await Assertions.Expect(page.Locator("#output div")).ToHaveTextAsync($"Name:{testFullName}");
+            await Assertions.Expect(page.Locator("#output div")).ToHaveTextAsync($"Name:{outputValue}");
         }
-        public async Task VerifyFullNameOutputCleared(string testFullName)
+
+        public async Task VerifyOutputCleared()
         {
-            await Assertions.Expect(page.Locator("#output div")).ToBeEmptyAsync(); //Not.ToHaveTextAsync($"Name:{testFullName}");
+            await Assertions.Expect(page.Locator("#output div")).ToBeEmptyAsync();
         }
     }
 }
--- PlaywrigthUITests/Tests/TextBoxTests.cs
+++ PlaywrigthUITests/Tests/TextBoxTests.cs
@@ -13,61 +13,63 @@ namespace PlaywrigthUITests.Tests
             _TextBoxPage = new PO_TextBoxPage(Page);
         }
 
-        //Test DATA:_______________________________________
-        public string testPageUrl = "https://demoqa.com/text-box";
-
-        private string testPageH1 = "Text Box";
+        //TEST DATA:_______________________________________
+        //Page:
+        public string PageUrl = "https://demoqa.com/text-box";
+        private string PageH1 = "Text Box";
+        //Labels:
         private string fullNamePlaceholder = "Full Name";
         private string fullNameLabel = "Full Name";
-
-        private string testFullName = "Test Name 123";
-        private string testEmail = "TestEmail@test.net";
-        private string testCurrentAddress = "Test Current Adress 123";
-        private string testPermanentAddress = "Test Permanent Address 123";
+        //Inputs:
+        private string FullName = "Test Name 123";
+        private string Email = "TestEmail@test.net";
+        private string CurrentAddress = "Test Current Adress 123";
+        private string PermanentAddress = "Test Permanent Address 123";
+        private string SubmitBtn = "Submit";
         //_________________________________________________
 
         [Test]
         [Description("TextBox Page H1 'Text Box' should be visible")]
         public async Task VerifyTextBoxPageH1()
         {
-            await _TextBoxPage.GoToTextBoxPage(testPageUrl);
-            await _TextBoxPage.IsTextBoxPageH1Visible(testPageH1);
+            await _TextBoxPage.GoToURL(PageUrl);
+            await _TextBoxPage.IsPageH1Visible(PageH1);
         }
 
         [Test]
         [Description("Label 'Full Name' should be visible")]
         public async Task VerifyFullNameLabel()
         {
-            await _TextBoxPage.GoToTextBoxPage(testPageUrl);
-            await _TextBoxPage.IsFullNameLabelVisible(fullNameLabel);
+            await _TextBoxPage.GoToURL(PageUrl);
+            await _TextBoxPage.IsInputLabelVisible(fullNameLabel);
         }
 
         [Test]
-        [Description("Enter {testFullName} in Full Name input, press submit, text Name should be 'Name:{testFullName}'")]
+        [Description("Enter {FullName} in Full Name input, press submit, text Name should be 'Name:{FullName}'")]
         public async Task VerifyFullNameFilled()
         {
-            await _TextBoxPage.GoToTextBoxPage(testPageUrl);
-            await _TextBoxPage.FillFullName(testFullName, fullNamePlaceholder);
-            await _TextBoxPage.IsFullNameFocused(fullNamePlaceholder);
-            await _TextBoxPage.ClickButton("Submit");
-            await _TextBoxPage.IsButtonFocused("Submit");
-            await _TextBoxPage.VerifyFullNameOutput(testFullName);
+            await _TextBoxPage.GoToURL(PageUrl);
+            await _TextBoxPage.FillInput(FullName, fullNamePlaceholder);
+            await _TextBoxPage.IsInputFocused(fullNamePlaceholder);
+            await _TextBoxPage.ClickButton(SubmitBtn);
+            await _TextBoxPage.IsButtonFocused(SubmitBtn);
+            await _TextBoxPage.VerifyOutputValue(FullName);
         }
 
         [Test]
-        [Description("Clear 'Full Name' Input, press submit, text 'Name:{testFullName} should not be visible")]
+        [Description("Clear 'Full Name' Input, press submit, text 'Name:{FullName} should not be visible")]
         public async Task VerifyFullNameInput()
         {
-            await _TextBoxPage.GoToTextBoxPage(testPageUrl);
-            await _TextBoxPage.FillFullName(testFullName, fullNamePlaceholder);
-            await _TextBoxPage.IsFullNameFocused(fullNamePlaceholder);
-            await _TextBoxPage.ClickButton("Submit");
-            await _TextBoxPage.IsButtonFocused("Submit");
-            await _TextBoxPage.VerifyFullNameOutput(testFullName);
-            
-            await _TextBoxPage.ClearFullNameInput(fullNamePlaceholder);
-            await _TextBoxPage.ClickButton("Submit");
-            await _TextBoxPage.VerifyFullNameOutputCleared(testFullName);
+            await _TextBoxPage.GoToURL(PageUrl);
+            await _TextBoxPage.FillInput(FullName, fullNamePlaceholder);
+            await _TextBoxPage.IsInputFocused(fullNamePlaceholder);
+            await _TextBoxPage.ClickButton(SubmitBtn);
+            await _TextBoxPage.IsButtonFocused(SubmitBtn);
+            await _TextBoxPage.VerifyOutputValue(FullName);
+
+            await _TextBoxPage.ClearInput(fullNamePlaceholder);
+            await _TextBoxPage.ClickButton(SubmitBtn);
+            await _TextBoxPage.VerifyOutputCleared();
         }
     }
 }
