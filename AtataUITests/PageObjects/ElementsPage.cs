5de99019dc1699e9fb2a848ec2bef835029517f1
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-06-25 13:02:35 +0300

    save

--- AtataUITests/PageObjects/ElementsPage.cs
+++ AtataUITests/PageObjects/ElementsPage.cs
@@ -4,6 +4,8 @@ using _ = AtataUITests.PageObjects.ElementsPage;
 namespace AtataUITests.PageObjects
 {
     [Url("elements")]
+    [VerifyTitle("DEMOQA")]
+    [VerifyContent("Please select an item from left to start practice.")]
     public sealed class ElementsPage : DemoQAPage<_>
     {
         [FindById("item-0")]
--- AtataUITests/Tests/ElementsTests.cs
+++ AtataUITests/Tests/ElementsTests.cs
@@ -5,16 +5,13 @@ namespace AtataUITests.Tests
     public sealed class ElementsTests : UITestFixture
     {
         //Elements:
-        //Preconditions: Go to https://demoqa.com/elements
-        //TC 1: Image "https://demoqa.com/images/Toolsqa.jpg" should be in the Header
-        //TC 2: Image "https://demoqa.com/images/Toolsqa.jpg" should be centered in the header
-        //TC 3: Image "https://demoqa.com/images/Toolsqa.jpg" should be a link to Home page "https://demoqa.com/"
 
         [Test]
         [Description("TestDecription")]
-        public void TestName1()
+        public void VerifyURL()
         {
-            Go.To<ElementsPage>();
+            Go.To<ElementsPage>()
+                .PageUrl.Should.Be("https://demoqa.com/elements");
         }
 
     }
